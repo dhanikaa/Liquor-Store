5b28e53a6f07c7342888fb7ebc44a62c777be7fb
dhanikaa <dhanikaanupama2000@gmail.com>
2024-02-26 10:46:19 +0530

    Initial commit

---
+++ src/main/java/com/architecture/Liquor/Store/LiquorStoreApplication.java
@@ -0,0 +1,13 @@
+package com.architecture.Liquor.Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquorStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquorStoreApplication.class, args);
+	}
+
+}
