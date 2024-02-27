7d0d8bf162c93240b2b03b10bee095af4253be44
dhanikaa <dhanikaanupama2000@gmail.com>
2024-02-28 03:26:56 +0530

    Added Dependencies

--- src/main/java/com/architecture/Liquor/Store/controller/UserController.java
+++ src/main/java/com/architecture/Liquor/Store/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.architecture.Liquor.Store.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value="api/v1/user")
@@ -12,7 +9,26 @@ public class UserController {
 
     @GetMapping("/getUser")
     public String getUser(){
-        return "Hello World";
+
+        return "User Created";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(){
+
+        return "User Saved";
+    }
+
+    @PutMapping("/updateUser")
+    public String updateUser(){
+
+        return "User Updated";
+    }
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser(){
+
+        return "User deleted";
     }
 
 }
