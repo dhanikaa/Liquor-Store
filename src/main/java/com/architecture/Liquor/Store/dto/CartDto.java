9295b608c800bfc5a4cd42192fbd94686257bb6a
dhanikaa <dhanikaanupama2000@gmail.com>
2024-02-28 23:45:27 +0530

    Created User DB

--- src/main/java/com/architecture/Liquor/Store/dto/CartDto.java
+++
@@ -1,9 +0,0 @@
-package com.architecture.Liquor.Store.dto;
-
-public class CartDto {
-    public int cartId;
-    public int userId;
-    public int productId;
-    public int quantity;
-    public int totalProductPrice;
-}
