2802402fd69e4ab0ab10b2a3134b465efeab8e03
dhanikaa <dhanikaanupama2000@gmail.com>
2024-02-28 15:06:58 +0530

    Created DTOs

---
+++ src/main/java/com/architecture/Liquor/Store/dto/OrderDto.java
@@ -0,0 +1,13 @@
+package com.architecture.Liquor.Store.dto;
+
+public class OrderDto {
+    private int orderId;
+
+    private int userId;
+
+    private int totalQuantity;
+
+    private int totalPrice;
+
+    private String dateTime;
+}
