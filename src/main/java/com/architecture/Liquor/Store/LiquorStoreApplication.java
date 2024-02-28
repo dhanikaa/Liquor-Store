ecbf432a0de45320b26d6a3e5d785f3e9325cc9d
dhanikaa <dhanikaanupama2000@gmail.com>
2024-02-28 23:45:04 +0530

    Created User Database

--- src/main/java/com/architecture/Liquor/Store/LiquorStoreApplication.java
+++ src/main/java/com/architecture/Liquor/Store/LiquorStoreApplication.java
@@ -1,9 +1,7 @@
 package com.architecture.Liquor.Store;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LiquorStoreApplication {
@@ -11,4 +9,5 @@ public class LiquorStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LiquorStoreApplication.class, args);
 	}
+
 }
