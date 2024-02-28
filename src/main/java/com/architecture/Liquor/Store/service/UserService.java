232ed2995304754201a88b29fc6d241f78a4026d
dhanikaa <dhanikaanupama2000@gmail.com>
2024-02-29 03:19:19 +0530

    Revert "Added CRUD Operations"
    This reverts commit d0ef0c4d31ef9a442325ee1cd62911fbcffc22c6.

--- src/main/java/com/architecture/Liquor/Store/service/UserService.java
+++ src/main/java/com/architecture/Liquor/Store/service/UserService.java
@@ -5,38 +5,18 @@ import com.architecture.Liquor.Store.entity.User;
 import com.architecture.Liquor.Store.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @Transactional
 public class UserService {
     @Autowired
     private UserRepository userRepository;
-
     @Autowired
     private ModelMapper modelMapper;
-
     public UserDto saveUser(UserDto userDto){
         userRepository.save(modelMapper.map(userDto, User.class));
         return userDto;
-    }
-
-    public List<UserDto> getAllUsers(){
-        List<User>userList=userRepository.findAll();
-        return modelMapper.map(userList,new TypeToken<List<UserDto>>(){}.getType());
-    }
-
-    public UserDto updateUser(UserDto userDto){
-        userRepository.save(modelMapper.map(userDto, User.class));
-        return userDto;
-    }
-
-    public boolean deleteUser(UserDto userDto){
-        userRepository.delete(modelMapper.map(userDto, User.class));
-        return true;
     }
 }
