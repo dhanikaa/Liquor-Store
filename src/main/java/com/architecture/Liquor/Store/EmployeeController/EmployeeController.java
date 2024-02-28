a511b9310cf74fddd4fd071cb35055bc5ea1a217
Dinusha Madushan <madaduw-se20042@stu.kln.ac.lk>
2024-02-28 13:22:31 +0530

    Add packages

---
+++ src/main/java/com/architecture/Liquor/Store/EmployeeController/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.architecture.Liquor.Store.EmployeeController;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.RegisterLogin.Dto.EmployeeDTO;
+import com.example.RegisterLogin.Dto.LoginDTO;
+import com.example.RegisterLogin.Service.EmployeeService;
+import com.example.RegisterLogin.response.LoginResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/employee")
+public class EmployeeController {
+    @PostMapping(path = "/save")
+    public String saveEmployee(@RequestBody EmployeeDTO employeeDTO)
+    {
+        String id = employeeService.addEmployee(employeeDTO);
+        return id;
+    }
+}
