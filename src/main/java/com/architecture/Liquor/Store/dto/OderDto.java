fd7fa0aa70a7ca9a0182f7447b83d31fb668942b
Gaveen_Buddhika <buddhik-se20022@stu.kln.ac.lk>
2024-02-29 14:46:50 +0530

    modify OderDto

--- src/main/java/com/architecture/Liquor/Store/dto/OderDto.java
+++ src/main/java/com/architecture/Liquor/Store/dto/OderDto.java
@@ -1,4 +1,22 @@
 package com.architecture.Liquor.Store.dto;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class OderDto {
+    @Id
+private int oid;
+    private int pid;
+    private int quantity;
+    private int price;
+
 }
+
