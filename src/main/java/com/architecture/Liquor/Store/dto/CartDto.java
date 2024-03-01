777439bfad12fdd5713b46186d3bbb7b3fa22097
Gaveen_Buddhika <buddhik-se20022@stu.kln.ac.lk>
2024-03-01 14:49:33 +0530

    modify cartDto and Oderdto

--- src/main/java/com/architecture/Liquor/Store/dto/CartDto.java
+++ src/main/java/com/architecture/Liquor/Store/dto/CartDto.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-@Entity
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
--- src/main/java/com/architecture/Liquor/Store/dto/OderDto.java
+++ src/main/java/com/architecture/Liquor/Store/dto/OderDto.java
@@ -7,7 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
