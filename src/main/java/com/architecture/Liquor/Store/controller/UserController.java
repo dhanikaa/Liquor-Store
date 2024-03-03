8f4d66b3e278205b64fbe6f833d56c8a8278e192
dhanikaa <dhanikaanupama2000@gmail.com>
2024-03-03 08:20:56 +0530

    Finalized the Backend

--- src/main/java/com/architecture/Liquor/Store/controller/UserController.java
+++ src/main/java/com/architecture/Liquor/Store/controller/UserController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     private final UserService userService;
