df54cf73090cea3db7ecfe14c23b325459b88286
dhanikaa <dhanikaanupama2000@gmail.com>
2024-03-03 10:15:13 +0530

    Update Order.java

--- src/main/java/com/architecture/Liquor/Store/entity/Order.java
+++ src/main/java/com/architecture/Liquor/Store/entity/Order.java
@@ -1,18 +1,17 @@
 package com.architecture.Liquor.Store.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "orders") // Specify the table name here
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Order {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int oid;
